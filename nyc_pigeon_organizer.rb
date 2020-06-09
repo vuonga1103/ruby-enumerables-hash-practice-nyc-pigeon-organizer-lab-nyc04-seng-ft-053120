b1b9125082d4a3a25248550c2b8b2173dbf62083
Anh Vuong <vuonga1103@yahoo.com>
2020-06-09 02:26:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -49,45 +49,20 @@
 #   }
 # }
 
-def nyc_pigeon_organizer(pigeon_data)
-
-  # Helper function that generates a list of pigeon names based on data
-  def name_list(pigeon_data)
-    result = []
-    # Loop through pigeon's data first value, which is an inner hash
-    pigeon_data.each_value do |inner_hash|
-      # Loop through the inner hash's values, which are name arrays
-      inner_hash.each_value do |name_array|
-        # Push each name encountered into result unless result already has that name
-        name_array.each { |name| result << name unless result.include?(name) }
-      end
-      # Break after the first loop
-      break
-    end
-
-    # Return the result array
-    result
-  end
-
-  # Helper function that generates a list of all characteristics present in pigeon data
-  def characteristic_list(pigeon_data)
-    pigeon_data.keys
-  end
-
-  #Helper function that generates an empty hash using list of names and list of characteristics
-  def empty_pigeon_list(name_list, characteristic_list)
-    result = {}
-    name_list.each do |name|
-      result[name] = {}
-      characteristic_list.each do |characteristic|
-        result[name][characteristic] = []
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  
+  data.each do |characteristic, inner_hash|
+    inner_hash.each do |attribute, name_array|
+      name_array.each do |name|
+        new_hash[name] = { characteristic = [] }
+        new_hash[name][characteristic] << attribute
       end
     end
-    result
   end
+  
 
-  pigeon_list = empty_pigeon_list(name_list(pigeon_data), characteristic_list(pigeon_data))
-  pigeon_list
+  new_hash
 end
 
 pigeon_data = {
